e94c2a5814686608e6f3adcaf87bd48df8fa230f
Robin Pelkner <robin.pelkner@tu-dortmund.de>
2018-11-12 18:36:57 +0100

    Massendurchsätze richtig mit Fehler

--- massendruchsatz.py
+++ massendruchsatz.py
@@ -6,6 +6,9 @@ from uncertainties import ufloat
 CW = ufloat(12500, 500)
 CK = ufloat(750, 0)
 L = ufloat(313.04285, 0)
-wert = ufloat(-0.0181, 0)
+wert = ufloat(-0.021, 0.002)
 massed = (CW + CK) / L * wert
 print(massed)
+
+x = np.sqrt(((12500 + 750)/313.04285)**2 * 0.002**2 + (1/313.04285 * -0.021)**2 * 500**2)
+print(x)
